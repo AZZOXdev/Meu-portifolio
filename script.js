dde5ef168244a5faaa11f5dcde86e3b13a3340fc
Tiago Antunes <ddhd746@gmail.com>
2023-09-18 12:24:58 -0300

    Responsive page

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function MenuShow(){
  let menuMobile = document.querySelector('.mobile-menu');
  if (menuMobile.classList.contains('open')){
-     menuMobile.classList.remove('open');
+     menuMobile.classList.remove('open')
+     document.querySelector('.M_icon').src = "Imagens/menu_white_36dp.png"
  }
  else{
-   menuMobile.classList.add('open');
+   menuMobile.classList.add('open')
+     document.querySelector('.M_icon').src = "Imagens/close_white_36dp.png"
  }
 }
