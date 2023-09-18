7a21dde231b68f5cb9ff6766e69add471cc1ee0e
Tiago Antunes <ddhd746@gmail.com>
2023-09-18 14:21:34 -0300

    Add-Button-scroll-top

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//menu hamburguer//
 function MenuShow(){
  let menuMobile = document.querySelector('.mobile-menu');
  if (menuMobile.classList.contains('open')){
@@ -9,3 +10,27 @@ function MenuShow(){
      document.querySelector('.M_icon').src = "Imagens/close_white_36dp.png"
  }
 }
+
+// fim menu hamburger//
+
+//botao up//
+
+const btn = document.getElementById('btnTop')
+
+btn.addEventListener('click', function(){
+  window.scrollTo(0,0)
+})
+
+document.addEventListener('scroll', ocultar)
+
+function ocultar (){
+  if(window.scrollY > 10){
+    btn.style.display = "flex"
+  }
+  else{
+    btn.style.display = "none"
+  }
+}
+
+ocultar()
+//fim botao up//
